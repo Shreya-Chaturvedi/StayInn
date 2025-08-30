5e8d945aafd6865fa0bc1dcb413b42594edadc98
Shreya Chaturvedi <shreya26chaturvedi@gmail.com>
2025-08-30 12:26:04 +0530

    changes in review.js

--- routes/review.js
+++ routes/review.js
@@ -3,24 +3,28 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js")
-const reviewController = require("../controllers/review.js")
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
+const reviewController = require("../controllers/review.js");
 
 //reviews
 //post reviews route
 router.post(
-    "/",
-    isLoggedIn,
-    validateReview,
-    wrapAsync(reviewController.createReview)
-  );
-  
-  //delete reviews route
-  router.delete(
-    "/:reviewId",
-    isLoggedIn,
-    isReviewAuthor,
-    wrapAsync(reviewController.destroyReview)
-  );
-  
-  module.exports = router;
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
+
+//delete reviews route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
